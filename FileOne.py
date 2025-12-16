aeb0019ad061722f8154b7f973b52f335cbf0f9f
Maximusss1 <132747851+Maximusss1@users.noreply.github.com>
2025-12-16 22:02:17 +0300

    Add Lession 2.1

--- FileOne.py
+++ FileOne.py
@@ -1,2 +1,28 @@
-print('Hellow world!')
-print('My name is Maxim!')
+# Lession 2.1
+# numbers = [3, 7, 5]
+# while True:
+#     number = int(input('Новое число: '))
+#     numbers.append(number)
+#     print('Текущий список чисел:', numbers)
+#     for i in numbers:
+#      print(i ** 2, i ** 3, i ** 4)
+#
+#     print()
+
+#Задача 2. Очень простая задача
+
+# numbers = [i for i in range(100)]
+# print(f'Созданный список: {numbers}')
+
+#Задача 3. Контроль
+# list_id = []
+# count_id = int(input('Введите кол-во людей в офисе: '))
+# for _ in range(count_id):
+#     new_id = int(input('ID сотрудника: '))
+#     list_id.append(new_id)
+# print(f'Новый список сотрудников: {list_id}')
+# search_id = int(input('Введите ID сотрудника: '))
+# if search_id in list_id:
+#     print('Сотрудник найден!')
+# else:
+#     print('Сотрудника нет')
