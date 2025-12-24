2123bd9df2b52d49c62fea8504a434e86ae7280c
Maximusss1 <132747851+Maximusss1@users.noreply.github.com>
2025-12-24 16:03:52 +0300

    Add Final HomeWork (3.0-3.10)

--- FileOne.py
+++ FileOne.py
@@ -224,3 +224,208 @@
 #         decode = []
 # print(f'Полученное сообщение: {list_packs}')
 # print('Кол-во ошибок в сообщении: ',list_packs.count(-1))
+
+# Задача 1. Матрица
+# matrix = [[1, 2 ,3], [4, 5, 6], [7, 8, 9]]
+# for index in matrix:
+#     for i in index:
+#         print(i,end=' ')
+#     print('')
+
+# Задача 2. Олимпиада
+# number_people = int(input('Кол-во участников: '))
+# number_teams = int(input('Кол-во команд: '))
+# if number_people % number_teams != 0:
+#     print(f'{number_people} участников не возможно поделить на команды по {number_teams} человек!')
+# else:
+#     num = 1
+#     all_list = []
+#     for peaople in range(number_people // number_teams):
+#         all_list.append(list(range(num, num + number_teams)))
+#         num += number_teams
+#     print(all_list)
+
+
+# Задача 3. Лавка
+# goods = [["яблоки", 50], ["апельсины", 190], ["груши", 100], ["нектарины", 200], ["бананы", 77]]
+# print(f'Текущий ассортимент: {goods}')
+# new_fruit = ['', 0]
+# new = input('Новый фрукт: ')
+# price = int(input('Цена: '))
+# goods.append([new,price])
+# print(f'Новый ассортимент: {goods}')
+# for frut in goods:
+#     frut[1] += frut[1] * 8 / 100
+#
+# print(f'Новый ассортимент с увел. ценой: {goods}')
+
+# def merge_sorted_lists(l1,l2):
+#     all_list = []
+#     for i in l1:
+#         if i not in l2:
+#             all_list.append(i)
+#     for i_2 in l2:
+#         if i_2 not in l1:
+#             all_list.append(i_2)
+#     return sorted(all_list)
+# list1 = [1, 3, 5, 7, 9]
+# list2 = [2, 4, 5, 6, 8, 10]
+# merged = merge_sorted_lists(list1, list2)
+# print(merged)
+
+#Final_HomeWork!
+#1
+# a = [1, 5, 3]
+# b = [1, 5, 1, 5]
+# c = [1, 3, 1, 5, 3, 3]
+# for i in b:
+#     a.append(i)
+# print('Количество цифр 5 при первом объединении: ',a.count(5))
+# a = [i for i in a if i != 5 ]
+# print(a)
+# for i in c:
+#     a.append(i)
+# print('Количество цифр 3 при втором объединении: ',a.count(3))
+# # a = [i for i in a if i != 3 ]
+# print('Итоговый список: ',a)
+
+#2
+# def merge_sorted_lists(l1,l2):
+#     all_list = sorted(list(set(l1 + l2)))
+#     return all_list
+# list1 = [1, 3, 5, 7, 9]
+# list2 = [2, 4, 5, 6, 8, 10]
+    # all_list = []
+    # all_list.extend(list1)
+    # all_list.extend(list2)
+    # for index in all_list:
+    #     if all_list.count(index) > 2:
+    #         all_list.remove(index)
+    # print(sorted(all_list))
+
+# merged = merge_sorted_lists(list1, list2)
+# print(merged)
+
+# #3
+# shop = [['каретка', 1200], ['шатун', 1000], ['седло', 300], ['педаль', 100], ['седло', 1500], ['рама', 12000], ['обод', 2000], ['шатун', 200], ['седло', 2700]]
+# name_detail = input('Введите название детали: ')
+# final_list = [0,0]
+# for i in shop:
+#     if i[0] == name_detail:
+#         final_list[0] += i[1]
+#         final_list[1] += 1
+#
+# if final_list[1] > 0:
+#     print(f'Кол-во деталей: {final_list[1]}')
+#     print(f'Общая стоимость деталей: {final_list[0]}')
+# else:
+#     print('Таких деталей нет в наличии.')
+
+#3
+# guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
+#
+# while True:
+#     print(f'\nСейчас на вечеринке {len(guests)} человек: {guests}')
+#     question = input('Гость пришёл или ушёл?')
+#     if question == 'пришёл':
+#         name_guest = input('Имя гостя: ')
+#         if len(guests) == 6:
+#             print('Прости',name_guest,',но мест нет.')
+#         else:
+#             guests.append(name_guest)
+#
+#     if question == 'ушёл':
+#         name_guest = input('Имя гостя: ')
+#         print('Пока',name_guest,'!')
+#         guests.remove(name_guest)
+#
+#     if question == 'пора спать':
+#         print('Вечеринка закончилась, все легли спать.')
+#         break
+
+#5
+# violator_songs = [
+#
+# ['World in My Eyes', 4.86],
+#
+# ['Sweetest Perfection', 4.43],
+#
+# ['Personal Jesus', 4.56],
+#
+# ['Halo', 4.9],
+#
+# ['Waiting for the Night', 6.07],
+#
+# ['Enjoy the Silence', 4.20],
+#
+# ['Policy of Truth', 4.76],
+#
+# ['Blue Dress', 4.29],
+#
+# ['Clean', 5.83]
+#
+# ]
+# music = 0
+# number = int(input('Сколько песен выбрать? '))
+# for i in range(number):
+#     print(f'Название {i + 1}-й песни: ',end='')
+#     name_m = input()
+#     for index in violator_songs:
+#         if index[0] == name_m:
+#             music += index[1]
+# print(f'Общее время звучания песен - {round(music,2)}')
+
+#6
+# list_rolls = []
+# count_rolls = int(input('Количество роликов? '))
+# for i in range(count_rolls):
+#     print(f'Размер пары {i + 1}',end='')
+#     size = int(input())
+#     list_rolls.append(size)
+# list_peaople = 0
+# count_peaople = int(input('Количество людей: '))
+# for index in range(count_peaople):
+#     print(f'Размер ноги человека {index + 1}:',end='')
+#     size_peaople = int(input())
+#     if size_peaople in list_rolls:
+#         list_peaople += 1
+#         list_rolls.remove(size_peaople)
+#
+# print('Наибольшее количество людей, которые могут взять ролики: ', list_peaople)
+
+#7
+# N = int(input('Кол-во людей: '))
+# K = int(input('Какое число в считалке: '))
+# people = [list(range(1,N + 1))]
+# out = 0
+# print(f'Выбывает каждый {K}-й человек')
+# print(f'Текущий круг людей: {people}')
+# for i in range(N-1):
+#     print('Текущий круг людей', people)
+#     start_count = out % len(people)
+#     out = (start_count + K - 1) % len(people)
+#     print('Начало счёта с номера', people[start_count])
+#     print('Выбывает человек под номером', people[out])
+#     people.remove(people[out])
+# print()
+# print('Остался человек под номером', people[0])
+#
+# #9
+# num_list = []
+# number_list = []
+# num_count = int(input('Количество чисел: '))
+# for _ in range(num_count):
+#     print('Число: ',end=' ')
+#     n = int(input(''))
+#     num_list.append(n)
+#
+# print(f'Последовательность: {num_list}')
+#
+# counter = 0
+# while num_list != num_list[::-1]:
+#     num_list.insert(num_count,num_list[counter])
+#     print(num_count)
+#     counter += 1
+#
+# print(f'Нужно приписать чисел: {counter}')
+# print(f'Сами числа: {num_list[:counter][::-1]}')
