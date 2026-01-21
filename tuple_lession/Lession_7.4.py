73919079c3af91489076f66149619263cf5227b5
Maximusss1 <132747851+Maximusss1@users.noreply.github.com>
2026-01-21 16:12:14 +0300

    Add lession 7.4,7.5. And final lession is tuple.

---
+++ tuple_lession/Lession_7.4.py
@@ -0,0 +1,39 @@
+# Задача 1. Кризис миновал
+# incomes = {
+#     'apple': 5600.20,
+#     'orange': 3500.45,
+#     'banana': 5000.00,
+#     'bergamot': 3700.56,
+#     'durian': 5987.23,   'peach': 10000.50,
+#     'pear': 1020.00,
+#     'persimmon': 310.00,
+#
+# }
+# for k,v in incomes.items():
+#     print(f'{k} - {v}')
+
+# Задача 2. Сервер
+# erver_data = {
+#     "server": {
+#         "host": "127.0.0.1",
+#         "port": "10"
+#             },
+#     "configuration": {
+#         "access": "true",
+#         "login": "Ivan",
+#         "password": "qwerty"
+#                     }
+#     }
+#
+# for key in erver_data.keys():
+#     print(f'{key}:')
+#     for value in erver_data[key]:
+#         print(f'   {value}: {erver_data[key][value]}')
+#     print()
+
+#Задача 3. В одну строчку
+# print([value for key,value in {0: 0, 1: 100, 2: 144, 3: 20, 4: 19}.items() if key % 2 == 0])
+
+
+
+
